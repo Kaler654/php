d7f88e5ab5794362df64dd5eab955701736abf33
kaler654 <kaler654@mail.ru>
2024-02-06 14:37:14 +0300

    Hello World

---
+++ lab1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <a class="header__logo">
+                <img src="logo.png" alt="Картинка" width="300px" height="100px">
+            </a>
+            <h1 class="header__title">Hello, World!</h1>
+        </header>
+        <main class="main">
+            <h2 class="main__title">
+                <?php
+                    echo "Hello, World!"
+                ?>
+            </h2>
+        </main>
+        <footer class="footer">
+            <h2 class="footer__title">footer</h2>
+        </footer>
+    </div>
+</body>
+
+</html>
